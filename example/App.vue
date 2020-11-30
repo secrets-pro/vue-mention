f252e76c918efffc9455a7754b7be6d6847b5e8f
xubowen <bowen.xu@bangdao-tech.com>
2020-11-30 14:10:02 +0800

    fix: 联集菜单

--- example/App.vue
+++ example/App.vue
@@ -13,16 +13,73 @@
     </vue-mention>
     <h3>内容</h3>
     <div style="white-space: pre">{{ text }}</div>
+    {{ value }}
+    {{ label }}
+    <div>
+      <vue-menu :menu="menu" v-model="value" :label.sync="label"></vue-menu>
+    </div>
   </div>
 </template>
 
 <script>
 import VueMention from "../src/components/VueMention/";
-
+import VueMenu from "../src/components/VueMenu";
 export default {
-  components: { VueMention },
+  components: { VueMention, VueMenu },
   data() {
     return {
+      value: [],
+      label: [],
+      menu: [
+        { name: "home", title: "home" },
+        {
+          name: "space",
+          title: "MY SPACE",
+          items: [
+            {
+              name: "space-1",
+              title: "space",
+              items: [
+                { name: "space-2-1", title: "space-2-1" },
+                { name: "space-2-2", title: "space-2-2" }
+              ]
+            },
+            {
+              name: "space-2",
+              title: "space2",
+              items: [
+                { name: "space-2-1", title: "space-2-1" },
+                { name: "space-2-2", title: "space-2-2" }
+              ]
+            }
+          ]
+        },
+        {
+          name: "INFOMATION",
+          title: "INFOMATION",
+          items: [
+            { name: "333", title: "333" },
+            {
+              name: "444",
+              title: "444",
+              items: [
+                { name: "space-2-1", title: "space-2-1" },
+                { name: "space-2-2", title: "space-2-2" }
+              ]
+            },
+            {
+              name: "555",
+              title: "555",
+              items: [
+                { name: "space-3-1", title: "space-3-1" },
+                { name: "space-3-2", title: "space-3-2" },
+                { name: "space-3-3", title: "space-3-2" },
+                { name: "space-3-4", title: "space-3-2" }
+              ]
+            }
+          ]
+        }
+      ],
       text: "Hello World! @Cyber Fei \n@mxsg.com @小明 ahaha",
       members1: ["Cyber Fei", "mxsg.com", "小明"],
       members2: [
---
+++ src/components/VueMenu/index.js
@@ -0,0 +1,7 @@
+import "./index.less";
+
+import component from "./index.vue";
+component.install = function(Vue) {
+  Vue.component(component.name, component);
+};
+export default component;
