055d5c977133e8950af66508e23ffbae12b45d68
tianxuanzhizi <bowen.xu@bangdao-tech.com>
2020-12-15 14:11:42 +0800

    fix: vue-menu

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 let env = process.env.NODE_ENV == "production";
 let externals = env
   ? {
-      lodash: "_",
-      "@secrets/vue-menu": "vue-menu"
+      lodash: "_"
+      // "@secrets/vue-menu": "vue-menu"
     }
   : undefined;
 module.exports = {
